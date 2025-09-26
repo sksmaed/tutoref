b03750b9ed48fadf30c081b52184fbd219ccb96f
ChangFuHsiang <shine0921873532@gmail.com>
2025-09-26 11:47:08 +0800

    Save current changes in feat/api

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState, useEffect, useRef, useCallback } from "react";
-import { useState, useEffect, useRef, useCallback } from "react";
 import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import styles from "./Navbar.module.css";
